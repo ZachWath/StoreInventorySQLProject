bc6f829079086b3276d9df407f0c4ddb790c0fdb
ZachWath <89270314+ZachWath@users.noreply.github.com>
2021-09-27 13:22:41 -0500

    added the add csv function , not completed yet, added some data cleaning functionality

--- app.py
+++ app.py
@@ -3,7 +3,52 @@ import datetime
 import csv
 import time
 
+def clean_price(price_str):
+    split_price_str = price_str.replace('$','')
+    price_float = float(split_price_str)
+    return int(price_float * 100)
+
+
+# def clean_quantity():
+
+
+def clean_date(date_str):
+    split_date = date_str.split('/')
+    month = int(split_date[0])
+    day = int(split_date[1])
+    year = int(split_date[2])
+    return_date = datetime.date(year, month, day)
+    return return_date
+
+
+
+def add_csv():
+    with open ('inventory.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            print (row)
+            product_name = row[0]
+            product_price = clean_price(row[1])
+            product_quantity = int(row[2])
+            product_date = clean_date(row[3])
+            new_product = Product(product_name=product_name, product_price=product_price,
+                                    product_quantity=product_quantity, product_date=product_date)
+            session.add(new_product)
+        session.commit()
+
+
+def app():
+    app_running = True
+    
+
+
+
+
 
 
 if __name__ == '__main__':
     Base.metadata.create_all (engine)
+    add_csv()
+
+    for product in session.query(Product):
+        print(product)
